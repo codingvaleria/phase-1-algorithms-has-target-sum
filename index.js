2a58270feee8384038850de8580fb430d4da5673
codingvaleria <codingvaleria@gmail.com>
2022-08-29 11:03:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let x = i + 1; x < array.length; x++) {
+      if (array[x] === complement) return true;
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+
+  0(n * n)
 */
+
 
 /* 
   Add your pseudocode here
+
+  iterate over the array of numbers identify X value that adds ups to the target. 
+check if X is the complement if so return true else false, also if its end of iteration return false.
+
 */
+
 
 /*
   Add written explanation of your solution here
+for an array iterate through each element in the length of array
+find the complement by confirming if the complement and array number add up to the target
+confirm if there is also a number equal to the complement within the array.
+
+
 */
 
 // You can run `node index.js` to view these console logs
